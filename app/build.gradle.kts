6e81c89e0518fd097572d33e0c99d26b7097d854
NATUREBRO <1479636987@qq.com>
2026-01-28 00:58:26 +0800

    优化了备份数据的体验, 新增了csv格式的导入数据, 新增了二级分类, 优化了部分UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.budgetapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "3.0.5"
+        versionCode = 27
+        versionName = "3.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
