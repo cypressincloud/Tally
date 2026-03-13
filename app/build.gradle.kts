8de65c0386eb58644dee0f46301d8c6506836c2b
NATUREBRO <1479636987@qq.com>
2026-03-13 19:57:03 +0800

    1, 添加明细模块

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.budgetapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 47
-        versionName = "3.2.6"
+        versionCode = 48
+        versionName = "3.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
